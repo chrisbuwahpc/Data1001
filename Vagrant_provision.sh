7e049599c325afd48a4cd57493b4b9b302748a97
Chris Bording <chris.bording@uwa.edu.au>
2021-07-27 12:18:14 +0800

    create build_master dir and updated .gitignore

--- Vagrant_provision.sh
+++ Vagrant_provision.sh
@@ -1,47 +1,6 @@
-# Migrated from Centos7 to Ubuntu20.04 ml_scripts
-# Done by Chris Bording
-#  Original Author Hideo Joho
-# Vagrant jupyter with R.md
-# Update package
-echo "Updating default packages ..."
-sudo apt -y update
-# Install dev tools
-#echo "Installing Development tools ..."
-#sudo apt -y groupinstall base "Development tools"
-#sudo apt -y install zlib-devel bzip2-devel openssl-devel sqlite-devel readline-devel
-
-# Disable SELinux and firewall
-#echo "Disabling SELinux and firewall ..."
-#sudo setenforce 0
-#sudo systemctl stop firewalld
-#sudo systemctl disable firewalld
-#sudo sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
-
-# Install Python and Jupyter notebook
-echo "Installing Python and Jupyter Notebook ..."
-# sudo apt -y install epel-release
-sudo apt -y install python3.8 python3.8-dev python3-distutils
-sudo apt -y install python3-widgetsnbextension python3-testresources jupyter-client
-sudo ln -s /usr/bin/python3 /usr/bin/python
-  #statements
-cd
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python3.8 get-pip.py
-sudo pip install ipython jupyter jupyter_contrib_nbextensions
-sudo pip install numpy scipy pandas matplotlib janome pyper
-cd
-jupyter contrib nbextension install --user
-
-# Install R and packages
-echo "Installing R and packages ..."
-sudo apt -y install r-base-core
-sudo apt -y install libczmq-dev libcurl4-openssl-dev libxml2 libxml2-dev 
-sudo apt -y install libssl-dev libfontconfig1-dev libharfbuzz-dev libfribidi-dev
-sudo apt -y install libfreetype6-dev libpng-dev libtiff5-dev libjpeg-dev
-sudo apt -y install libudunits2-dev libcairo2-dev
-sudo R -e "install.packages(c('crayon', 'pbdZMQ', 'devtools', 'ggplot2'), repos = 'https://cran.rstudio.com/', dep = TRUE)"
-sudo R -e "devtools::install_github(paste0('IRkernel/', c('repr', 'IRdisplay', 'IRkernel')))"
-sudo R -e "IRkernel::installspec(user = FALSE)"
+# 
+# Written by Chris Bording - HPC Manager
+# UWA IT - Infrastructures and Platforms - HPC Team 
 
 # Start Jupyter Notebook
 echo " "
